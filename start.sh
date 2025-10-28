053ed28da20cdeaa5fa4639da33dd04ee20fe905
Your Name <your_email@example.com>
2025-10-28 21:17:38 +0600

    Force push all files to master

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+java -Xms256M -Xmx1G \
+  -XX:+UseG1GC \
+  -XX:+ParallelRefProcEnabled \
+  -XX:MaxGCPauseMillis=200 \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+DisableExplicitGC \
+  -XX:G1NewSizePercent=30 \
+  -XX:G1MaxNewSizePercent=40 \
+  -XX:G1HeapRegionSize=8M \
+  -XX:G1ReservePercent=20 \
+  -XX:G1HeapWastePercent=5 \
+  -Djava.net.preferIPv4Stack=true \
+  -jar bukkit.jar nogui
